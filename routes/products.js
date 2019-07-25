bcda288e2caa7ad9e1a61424661eed81be71aba2
Kedra, Krystian <krystian.kedra@atos.net>
2019-07-25 21:39:18 +0200

    Created post method for adding new product with validation

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
 const verify = require('./verifyToken');
+const { productValidation } = require('../validation/validation');
+const Product = require('../models/Product');
 
 router.get('/', verify , async (req, res) => {
     try {
         res.send('Products')
     } catch (err) {
         res.send(err);
+    }
+});
+
+router.post('/', verify, async (req, res) => {
+    const { error } = productValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const isCorrectUserId = req.user._id === req.body.userId
+    if (!isCorrectUserId) return res.status(400).send('Invalid userId');
+
+    const product = new Product({
+        userId: req.body.userId,
+        title: req.body.title,
+        description: req.body.description
+    })
+
+    try {
+        await product.save();
+        res.status(200).send(product);
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
